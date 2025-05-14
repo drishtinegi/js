ebe49a82e5a3bc19aee806210b3f2d7134cf533a
drishtinegi <drishtinegi05@gmail.com>
2025-05-14 12:55:24 +0000

    data types in js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all JS code as new version of JS 
+
+// alert(3+ 3) //we are using nodejs, not browser
+
+console.log(3 +
+     3); console.log("hhh") //code readiblility should be high , avoid this
+
+
+let name = "Drishti" // string datatpe
+let age = 18 //integer datatype
+let isLoggedIn= false //boolean datatype
+
+//number => 2 to power 53
+//bighint //very large number
+//string => ""
+
+//boolean => true/false
+
+//null => standalone value //representation of empty value
+//undefined => //variable declared but value not assigned
+
+//symbol => unique
+
+//object
+
+console.log(typeof "Drishti"); //string
+console.log(typeof null); //give output as object
+console.log(typeof undefined); //give output as undefined
+
+
+
